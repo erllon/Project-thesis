6f85910ffde39c180e9e5d2f3a3dca4cb3a8206c
erllon <lone.erlend@gmail.com>
2020-12-02 15:11:34 +0100

    Final formation ok

--- Inspo_Magnus/main.py
+++ Inspo_Magnus/main.py
@@ -57,13 +57,6 @@ if __name__ == "__main__":
       )
 
   base_pos = np.array([-16,-16])
-  # obstacles = [
-  #   np.array([
-  #     (0.26-0.5, -0.80),
-  #     (0.26-0.5,  0.80),
-  #     (0.25-0.5,  0.80),
-  #     (0.25-0.5, -0.80)
-  #   ])]
   obstacles = [
     # np.array([
     #   (-6, -6),
@@ -138,6 +131,113 @@ if __name__ == "__main__":
     ]),obstacles#obstacles2#obstacles2#[] #obstacles
   )
 
+  ############################################
+  ############## 3 DRONES ####################
+  ############################################
+  N_agents =  3# 3,5funker fint med stor obstacle, bruk disse 6 funker også, men blir små lyseblå "merker" slik som tidligere, 4,7 funker ikke
+  #3 funker ok
+  #3,4 funker ok, 5 funker ikke
+
+  sensing_r = 4
+  comm_r = 2*sensing_r#2*sensing_r
+
+  agents = [
+    Agent(
+      base_pos + np.array([(i+1)*0.05, (i+1)*0.05]),#+ np.random.uniform(0, 0.16, 2), #np.array([-16, -16]) + np.random.uniform(0, 0.16, 2),
+      F, sensing_radius=sensing_r, communication_radius=comm_r)
+    for i in range(N_agents)]
+  for agent in agents:
+    print(f"pos:{agent.s}")
+  for agent in agents:
+    agent.other_agents = [a for a in agents if a != agent]
+    agent.compute_opt_dependencies()
+  initial_cover = get_covered_polygon(F, agents) #Not used
+  
+  fig, axs = plt.subplots(2, num=1)
+  
+  axs[0].set_title("Final formation, 3 drones")
+  axs[0].axis('equal')
+
+  F.plot(axs[0])
+  # for agent in agents:
+  #   agent.plot(axs[0])
+  
+  hist_obj_val = np.zeros(1)
+  cov_area_3 = np.zeros(1)
+  obj_val = 0
+  while not np.array([agent.converged for agent in agents]).all():
+    for agent in agents:
+      agent.do_step()
+      cov_area_3 = np.append(cov_area_3, get_covered_polygon(F,agents).area)
+      obj_val = 0
+      for a in agents:
+        obj_val += agent.get_objective_at_point(a.s)
+      hist_obj_val = np.append(hist_obj_val, obj_val)
+  print(f"hist_obj_val:\n{hist_obj_val}")
+  print(f"cov_area_3:\n{cov_area_3}")
+
+
+    
+  axs[1].set_title("Final formation, 5 drones")
+  axs[1].axis('equal')
+  F.plot(axs[0])
+  positions = []
+  for agent in agents:
+    agent.plot(axs[0])
+    positions.append(agent.s)
+  axs[0].plot(base_pos[0], base_pos[1],"r^")
+  
+
+# %% Agents
+  for agent in agents:
+    if agent.compute_c(base_pos) == 1:
+      x_values_b = [agent.s[0], base_pos[0]]
+      y_values_b = [agent.s[1], base_pos[1]]
+    
+      axs[0].plot(x_values_b, y_values_b, 'b')
+    for other in agent.other_agents:
+      if agent.compute_c(other.s) == 1:
+        agent.neighboors.append(other.node_id)
+        x_values = [agent.s[0], other.s[0]]
+        y_values = [agent.s[1], other.s[1]]
+    
+        axs[0].plot(x_values, y_values, 'b')
+
+
+  fig2, axs2 = plt.subplots(1,2, num=2,clear=True)
+  axs2[0].set(xlim=(-4,90), ylim=(-10,225))
+  axs2[0].set(xlim=(-4,90), ylim=(-10,225))
+
+  axs2[0].set_title("Covered area, 3 drones")
+  axs2[1].set_title("Covered area, 5 drones")
+
+  axs2[0].plot(range(len(cov_area_3)), cov_area_3)
+
+  fig3, axs3 = plt.subplots(num=3,clear=True)
+  
+  axs3.set_title("Cost function")
+  axs3.plot(range(len(hist_obj_val)), hist_obj_val)
+
+
+  final_cover = get_covered_polygon(F, agents)
+  if isinstance(final_cover, Polygon):
+    x, y = final_cover.exterior.xy
+    axs[0].fill(x, y, color="blue", alpha=0.5)
+  else:
+    for geom in final_cover.geoms:
+      x, y = geom.exterior.xy
+      axs[0].fill(x, y, color="blue", alpha=0.5)
+  print(f"final covered area: {final_cover.area}")
+  print(f"total possibble coverage: {N_agents * np.pi * sensing_r**2}")
+
+  print("************FERDIG MED SIMULERING FOR 3 DRONER***********")
+  print("*************BEGYNNER SIMULERING MED 5 DRONER************")
+
+  #axs[
+  ############################################
+  ############## 5 DRONES ####################
+  ############################################
+
   N_agents =  5# 3,5funker fint med stor obstacle, bruk disse 6 funker også, men blir små lyseblå "merker" slik som tidligere, 4,7 funker ikke
   #3 funker ok
   #3,4 funker ok, 5 funker ikke
@@ -157,109 +257,77 @@ if __name__ == "__main__":
     agent.compute_opt_dependencies()
   initial_cover = get_covered_polygon(F, agents) #Not used
   
+  fi4, axs4 = plt.subplots(2, num=4)
   
-  fig, axs = plt.subplots(2)
-  
-  axs[0].set_title("Initial")
-  axs[0].axis('equal')
+  axs4[0].set_title("Initial")
+  axs4[0].axis('equal')
 
-  F.plot(axs[0])
-  for agent in agents:
-    agent.plot(axs[0])
+  # F.plot(axs4[0])
+  # for agent in agents:
+  #   agent.plot(axs4[0])
   
-
-  #obj_val = 0
   hist_obj_val = np.zeros(1)
-  cov_area = np.zeros(1)
+  cov_area_5 = np.zeros(1)
   obj_val = 0
   while not np.array([agent.converged for agent in agents]).all():
     for agent in agents:
       agent.do_step()
-      cov_area = np.append(cov_area, get_covered_polygon(F,agents).area)
+      cov_area_5 = np.append(cov_area_5, get_covered_polygon(F,agents).area)
       obj_val = 0
       for a in agents:
         obj_val += agent.get_objective_at_point(a.s)
       hist_obj_val = np.append(hist_obj_val, obj_val)
-      # obj_val = 0
-      # for a in agents:
-      #   obj_val += agent.objective_val
-      #hist_obj_val = np.append(hist_obj_val, obj_val)
   print(f"hist_obj_val:\n{hist_obj_val}")
-  print(f"cov_area:\n{cov_area}")
+  print(f"cov_area_5:\n{cov_area_5}")
+
 
     
-  colors = ['g','r','c','m','y','k']
-  axs[1].set_title("Final")
-  axs[1].axis('equal')
+  axs4[1].set_title("Final")
+  axs4[1].axis('equal')
+  # F.plot(axs4[1])
   F.plot(axs[1])
-  positions = []
-  for agent in agents:
-  #for i in range(len(agents)):
-    #agents[i].plot(axs[1], colors[i])
-    agent.plot(axs[1])
-    #positions.append(agents[i].s)
-    positions.append(agent.s)
-  axs[1].plot(base_pos[0], base_pos[1],"r^")
-    # agent.compute_opt_dependencies()
-  
-  # for i in range(len(agents)):
-  #   print(f"Pos{i}: {agents[i].s}")
-  #   # print(f"final covered area: {final_cover.area}")
-  # pos_agents = []
+
+  # positions = []
   # for agent in agents:
-  #   pos_agents.append(agent.s)
+  #   # agent.plot(axs4[1])
+  #   agent.plot(axs[0])
 
-# %% Base
-# for agent in agents:
+  #   positions.append(agent.s)
+  # axs4[1].plot(base_pos[0], base_pos[1],"r^")
+  axs[1].plot(base_pos[0], base_pos[1],"r^")
 
-#   pos = closest_node(base_pos,positions)
-
-#   x_values = [base_pos[0], pos[0]]
-#   y_values = [base_pos[1], pos[1]]
-
-#   axs[1].plot(x_values, y_values, 'b')
-  
+  for agent in agents:
+    agent.plot(axs[1])
+    positions.append(agent.s)
+  axs[1].plot(base_pos[0], base_pos[1],"r^")  
 
 # %% Agents
   for agent in agents:
-    #agent.compute_c()
-    # conn = []
-    # other_positions = []
     if agent.compute_c(base_pos) == 1:
       x_values_b = [agent.s[0], base_pos[0]]
       y_values_b = [agent.s[1], base_pos[1]]
     
+      # axs4[1].plot(x_values_b, y_values_b, 'b')
       axs[1].plot(x_values_b, y_values_b, 'b')
+
     for other in agent.other_agents:
-      if agent.compute_c(other.s) == 1:# and agent.in_FoV(other.s) == True:
+      if agent.compute_c(other.s) == 1:
         agent.neighboors.append(other.node_id)
         x_values = [agent.s[0], other.s[0]]
         y_values = [agent.s[1], other.s[1]]
     
+        # axs4[1].plot(x_values, y_values, 'b')
         axs[1].plot(x_values, y_values, 'b')
-      #other_positions.append(other.s)
-    #pos = closest_node(agent.s, other_positions)
-    
-    # x_values = [agent.s[0], pos[0]]
-    # y_values = [agent.s[1], pos[1]]
-    # agent.compute_opt_dependencies()
-    # neighbor_positions = []
-    # for neigh_idx in agent.B:
-    #   neigh_agent = agents[neigh_idx]
-    #   x_values = [agent.s[0], neigh_agent.s[0]]
-    #   y_values = [agent.s[1], neigh_agent.s[1]]
-    #   axs[1].plot(x_values, y_values, 'b')
-
-    # axs[1].plot(x_values, y_values, 'b')
 
 
-  fig2, axs2 = plt.subplots(num=2,clear=True)
-  axs2.set_title("Covered area")
-  axs2.plot(range(len(cov_area)), cov_area)
 
-  fig3, axs3 = plt.subplots(num=3,clear=True)
-  axs3.set_title("Cost function")
-  axs3.plot(range(len(hist_obj_val)), hist_obj_val)
+  fig5, axs5 = plt.subplots(num=5,clear=True)
+  axs5.set_title("Covered area")
+  axs5.plot(range(len(cov_area_5)), cov_area_5)
+  axs2[1].plot(range(len(cov_area_5)), cov_area_5)
+  fig6, axs6 = plt.subplots(num=6,clear=True)
+  axs6.set_title("Cost function")
+  axs6.plot(range(len(hist_obj_val)), hist_obj_val)
 
 
   final_cover = get_covered_polygon(F, agents)
@@ -270,7 +338,6 @@ if __name__ == "__main__":
     for geom in final_cover.geoms:
       x, y = geom.exterior.xy
       axs[1].fill(x, y, color="blue", alpha=0.5)
-  #print(f"initial covered area: {initial_cover.area}")
   print(f"final covered area: {final_cover.area}")
   print(f"total possibble coverage: {N_agents * np.pi * sensing_r**2}")
 
